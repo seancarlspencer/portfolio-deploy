8da6041def5f938c4a3bdcd58386ea3468c8f71d
seancarlspencer <47134137+seancarlspencer@users.noreply.github.com>
2020-08-25 01:16:45 -0400

    Updates

---
+++ precache-manifest.90d08cb5a1b0ef7163014180f64b684c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d34f2ea1ed59008f38d9da7ec5653b8",
+    "url": "/portfolio-deploy/index.html"
+  },
+  {
+    "revision": "37ef23d028b786b1a168",
+    "url": "/portfolio-deploy/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "b4645e6afc48566d9963",
+    "url": "/portfolio-deploy/static/js/2.23369472.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-deploy/static/js/2.23369472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37ef23d028b786b1a168",
+    "url": "/portfolio-deploy/static/js/main.e95caee7.chunk.js"
+  },
+  {
+    "revision": "80c73cd44813848130b2",
+    "url": "/portfolio-deploy/static/js/runtime-main.090c7182.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio-deploy/static/media/logo.ee7cd8ed.svg"
+  }
+]);
